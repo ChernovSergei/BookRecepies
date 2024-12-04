b1e63dc36d06d4b9ef5b2beff4f345c904be9a44
Sergei Chernov <goldengrail@gmail.com>
2024-12-04 12:39:21 +0500

    Controller refactoring - format had been added to console output to eliminate extra long code rows. Receipt format had been located outside saveRecepie() method.

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,7 +1,13 @@
 public class Controller {
+    private String recepieFormat = "%s the %s with a %s\n";
 
     public void saveRecepie(Recepie recepie) {
         System.out.println(recepie.getName());
-        recepie.getSteps().forEach(e -> System.out.println ( e.getAction().getName() + " the " + e.getProduct().getName() + " with a " + e.getTool().getName()));
+        recepie.getSteps().forEach(e -> {
+            String actionName = e.getAction().getName();
+            String productName = e.getProduct().getName();
+            String toolName = e.getTool().getName();
+            System.out.printf (recepieFormat, actionName, productName, toolName);
+        });
     }
 }
