66ff833ee56efc2fd57d9ab80a1294fe82deac6f
Sergei Chernov <goldengrail@gmail.com>
2024-12-25 15:22:29 +0500

    Second class with Ivan. Main method refactory- createRecepie, createRecepieSteps and CreateRecepieStep methods were extracted from the main method.

--- src/main/java/ConsoleClient.java
+++ src/main/java/ConsoleClient.java
@@ -2,20 +2,43 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ConsoleClient {
+
+    //TODO: Create two branches for above mentioned modifications (1 hr)
+    //TODO: Refactor ConsoleClient class (4 hr)
+    //TODO: Add file storage to saveRecepie (e.g. JSON) (6 hr)
+    //TODO: Time estimation skill - estimate required hours for those modifications
+    //TODO: Find good book about Refactoring from Ivan
+    private static final Input input = new ConsoleInput();
+
     public static void main(String[] args) {
-        Input consoleInput = new ConsoleInput();
-        String productName = consoleInput.askString("Enter the product name");
-        String productType = consoleInput.askString("Enter the product type name");
-        String tool = consoleInput.askString("Enter the tool name");
-        String action = consoleInput.askString("Enter the action name");
+        Recepie recepie = createRecepie();
+        Controller controller = new Controller();
+        controller.saveRecepie(recepie);
+    }
+
+    private static Recepie createRecepie() {
+        List<RecepieStep> recepieSteps = createRecepieSteps();
+        String recepieName = input.askString("Enter the recepie name");
+        Recepie recepie = new Recepie(recepieSteps, recepieName);
+        return recepie;
+    }
+
+    private static List<RecepieStep> createRecepieSteps() {
+        RecepieStep recepieStep = createRecepieStep();
+        List<RecepieStep> recepieSteps = new LinkedList<>();
+        recepieSteps.add(recepieStep);
+        return recepieSteps;
+    }
+
+    private static RecepieStep createRecepieStep() {
+        String productName = input.askString("Enter the product name");
+        String productType = input.askString("Enter the product type name");
+        String tool = input.askString("Enter the tool name");
+        String action = input.askString("Enter the action name");
         Product userProduct = new Product(productName, productType);
         Tool userTool = new Tool(tool);
         Action userAction = new Action(action);
-        RecepieStep cutTheMeat = new RecepieStep(userProduct, userTool, userAction);
-        List<RecepieStep> boiledMeatSteps = new LinkedList<>();
-        boiledMeatSteps.add(cutTheMeat);
-        Recepie boiledMeat = new Recepie(boiledMeatSteps, "Boiled Meat");
-        Controller controller = new Controller();
-        controller.saveRecepie(boiledMeat);
+        RecepieStep recepieStep = new RecepieStep(userProduct, userTool, userAction);
+        return recepieStep;
     }
 }
