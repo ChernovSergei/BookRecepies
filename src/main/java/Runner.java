bdace0121370644e14f67057ec84efbdd1b59dda
Sergei Chernov <goldengrail@gmail.com>
2025-02-01 18:48:55 +0400

    Tests for single and multiple recepies read/write to storage file had been added.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,14 +6,13 @@ public class Runner {
     //TODO: Separate client and server locally. There should be two separate apps (console and server)
     //TODO: Time estimation skill - estimate required hours for those modifications
     //TODO: Find good book about Refactoring from Ivan
+    //TODO: Question - is it good practice to combine all classes Action/Tool/Product into one package
     public static void main(String[] args) {
         ConsoleClient input = new ConsoleClient();
         input.createRecepie();
         input.addRecepieStep();
-        input.addRecepieStep();
         input.saveRecepie();
         input.createRecepie();
-        input.addRecepieStep();
         input.addRecepieStep();
         input.saveRecepie();
         input.printAllRecepies();
