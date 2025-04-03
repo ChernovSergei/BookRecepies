560da650cd3786abcd9a5d06f50b4ee2d37a67df
Sergei Chernov <goldengrail@gmail.com>
2025-04-03 09:33:38 +0500

    Receipes were sent in JSON format to the simple http server

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,29 +6,29 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Runner{
-    //TODO: Manage several recepies creation which will be stored in one file.
-    // New receipes could be added without overwritting
     //TODO: Add Auto Tests for Receipes creation.
     //TODO: Separate client and server locally. There should be two separate apps (console and server)
     //TODO: Time estimation skill - estimate required hours for those modifications
     //TODO: Find good book about Refactoring from Ivan
-
     //TODO: Question - is it good practice to combine all classes Action/Tool/Product into one package
     public static void main(String[] args) throws IOException{
-        StringBuilder responseBuilder = new StringBuilder();
         String storagePath = "path";
         String storageName = "Recepies.txt";
         RecepiesStorage recepiesStorage = new ReceiptsJSONFileStorage(storagePath, storageName);
         List<Recepie> recepies = recepiesStorage.getAll();
-        recepies.forEach(responseBuilder::append);
-        String response = responseBuilder.toString();
+        List<String> jsonRecepies = recepies.stream()
+                .map(JSONRecepieConverter::recepieToJson)
+                .collect(Collectors.toList());
+        String response = "[" + String.join(",", jsonRecepies) + "]";
         int port = 80;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/all", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
+                exchange.getResponseHeaders().set("Content-Type", "application/json");
                 exchange.sendResponseHeaders(200, response.getBytes().length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
